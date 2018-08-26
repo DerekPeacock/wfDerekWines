e1b497bcc8de8ef2266f688ceaa03cb2e76cad4e
DerekPeacock <derek.peacock@yahoo.com>
2018-08-26 19:10:54 +0100

    Add project files.

---
+++ wfDereksWines/Regions.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Regions.aspx.cs" Inherits="wfDereksWines.Regions" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ wfDereksWines/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wfDereksWines.Startup))]
+namespace wfDereksWines
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

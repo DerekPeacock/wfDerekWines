c70301830b252e0c48f48a3c8bd6c991063ce5c4
DerekPeacock <derek.peacock@yahoo.com>
2018-09-04 19:33:22 +0100

    Shopping cart complete with cart display and updates

--- wfDereksWines/Site.Master.cs
+++ wfDereksWines/Site.Master.cs
@@ -7,6 +7,7 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using wfDereksWines.BusinessLogic;
 
 namespace wfDereksWines
 {
@@ -70,6 +71,15 @@ namespace wfDereksWines
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr = string.Format("Cart ({0})", usersShoppingCart.GetCount());
+                cartCount.InnerText = cartStr;
+            }
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
